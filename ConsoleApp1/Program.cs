26b29f62a99417f056879a959f3b8b6ae713170c
s26922 <s26922@pjwstk.edu.pl>
2024-03-11 09:32:55 +0100

    Dodano zadanie 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,17 +3,17 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
-        //Console.WriteLine(GetAverage(new []{1,2,4,6,6}));
+        Console.WriteLine(GetAverage(new []{1,2,4,6,6}));
     }
 
-    // public static int GetAverage(int[] arr)
-    // {
-    //     var x = 0;
-    //     foreach (var i in arr)
-    //     {
-    //         x += i;
-    //     }
-    //
-    //     return x / arr.Length;
-    // }
+    public static int GetAverage(int[] arr)
+    {
+        var x = 0;
+        foreach (var i in arr)
+        {
+            x += i;
+        }
+    
+        return x / arr.Length;
+    }
 }
