227199c60b0acc38075d6600a03c8972983c5fc7
s26922 <s26922@pjwstk.edu.pl>
2024-03-11 09:26:31 +0100

    Modyfikacja 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,17 @@
 {
     public static void Main(string[] args)
     {
-        
+        Console.WriteLine(GetAverage(new []{1,2,4,6,6}));
+    }
+
+    public static int GetAverage(int[] arr)
+    {
+        var x = 0;
+        foreach (var i in arr)
+        {
+            x += i;
+        }
+
+        return x / arr.Length;
     }
 }
