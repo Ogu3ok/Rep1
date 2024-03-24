34728bd6890bd2e368ec33cae6cff589b5357de3
MrAnd6 <133496934+MrAnd6@users.noreply.github.com>
2024-03-24 22:16:46 +0100

    Finished task

---
+++ ConsoleApp2/Models/Kontenerowca.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp2.Models;
+
+public class Kontenerowca
+{
+    private List<ContainerBase> conteners;
+    private int maxPrzedkosc;
+    private int maxNKont;
+    private double maxWeight;
+
+    public Kontenerowca(int maxPrzedkosc, int maxNKont, double maxWeight)
+    {
+        this.maxPrzedkosc = maxPrzedkosc;
+        this.maxNKont = maxNKont;
+        this.maxWeight = maxWeight;
+    }
+
+    public void addCont(ContainerBase container)
+    {
+        conteners.Add(container);
+    }
+
+    public void removeCont(ContainerBase container)
+    {
+        conteners.Remove(container);
+    }
+
+    public void replaceCont(int idx, ContainerBase c2)
+    {
+        conteners.RemoveAt(idx);
+        conteners.Insert(idx, c2);
+    }
+
+    public void moveKont(int idx, Kontenerowca target)
+    {
+        ContainerBase cont = conteners[idx];
+        target.addCont(cont);
+        conteners.Remove(cont);
+    }
+
+    public override string ToString()
+    {
+        String text = $"Kontenerowca[Maksymalna przedkosc: {maxPrzedkosc}\nMaksymalna licba prewozonych kontenerow: {maxNKont}\nDopuszcalna masa: {maxWeight}]\n";
+        foreach (var container in conteners)
+        {
+            text += container.ToString();
+        }
+        return text;
+    }
+}
